a927c694184d9067888fbdee00574382d5ce2b02
santiago maza <mazasantiago.10@gmail.com>
2023-08-14 19:29:38 -0300

    get a productos, resolver vista

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -7,6 +7,7 @@ import {
   profile
 } from "../controllers/views.controllers.js";
 import { logoutUser } from "../controllers/user.controllers.js";
+import { getAll } from "../controllers/product.controllers.js";
 
 
 const router = Router();
@@ -19,7 +20,7 @@ router.get("/error-login", errorLogin);
 
 router.get("/error-register", errorRegister);
 
-router.get('/profile', profile)
+router.get('/profile', getAll)
 
 router.get('/logout', logoutUser)
 
